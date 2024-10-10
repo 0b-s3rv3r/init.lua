84941df20aac37a4948e0a076689e0702ba813b5
0b-s3rv3r <patrykstruzek@proton.me>
2024-10-10 22:43:30 +0200

    Restore mini.pairs

--- init.lua
+++ init.lua
@@ -361,7 +361,7 @@ require('lazy').setup {
     config = function()
       require('mini.ai').setup { n_lines = 500 }
       require('mini.surround').setup()
-      -- require('mini.pairs').setup()
+      require('mini.pairs').setup()
 
       local statusline = require 'mini.statusline'
 
